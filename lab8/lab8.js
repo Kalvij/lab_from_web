fe11f0f29e246594000e6a8ab285d111eb60e216
Kalvij <yefimkina04@mail.ru>
2024-06-14 14:19:09 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -14,3 +14,21 @@ function showDate(){
     out5.innerHTML = 'Дата и время для локали Италии: ' + today.toLocaleString('it-IT');
     out6.innerHTML = 'Дата и время для локали Автралии: ' + today.toLocaleString('en-AU');
 }
+function showDateTime() {
+    let currentDate = new Date();
+    let dateTime = document.getElementById('date-time');
+    let year = document.getElementById('year');
+    let month = document.getElementById('month');
+    let date = document.getElementById('date');
+    let dayOfWeek = document.getElementById('day-of-week');
+
+    dateTime.textContent = currentDate.toLocaleString();
+    year.textContent = currentDate.getFullYear();
+    month.textContent = currentDate.getMonth() + 1; 
+    date.textContent = currentDate.getDate();
+    dayOfWeek.textContent = DayOfWeekName(currentDate.getDay());
+}
+function DayOfWeekName(dayIndex) {
+    const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    return daysOfWeek[dayIndex];
+}
